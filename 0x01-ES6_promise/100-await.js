d705a90643b340f98e1be1028b62deb424eb4a99
bini34 <biniyamambachew@gmail.com>
2024-07-24 23:45:26 +0300

    update task 10

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils.js';
-export default async syncUploadUser(){
-	try{
-		const photo = await uploadPhoto();
-		const user = await createUser();
-		return { photo, user };
-	}
-	catch(error){
-		return { photo: null, user: null };
-	}
+
+export default async function asyncUploadUser() {
+    try {
+        const photo = await uploadPhoto();
+        const user = await createUser();
+        return { photo, user };
+    } catch (error) {
+        return { photo: null, user: null };
+    }
 }
